f34c3390c079c6849eabd1ff8da65b6b67a2eaa7
veshutov <veshutov98@gmail.com>
2025-08-20 09:33:01 +0300

    some logs

--- threads/src/main.zig
+++ threads/src/main.zig
@@ -20,13 +20,14 @@ fn listenTcp(address: net.Address) !void {
     try posix.bind(listener, &address.any, address.getOsSockLen());
     const backlog = 128;
     try posix.listen(listener, backlog);
+    std.debug.print("TCP server started on {}\n", .{address});
 
     var client_address: net.Address = undefined;
     var client_address_len: posix.socklen_t = @sizeOf(net.Address);
     while (true) {
         const socket = try posix.accept(listener, &client_address.any, &client_address_len, 0);
         errdefer posix.close(socket);
-        std.debug.print("{} connected\n", .{client_address});
+        std.debug.print("TCP {} connected\n", .{client_address});
         _ = try std.Thread.spawn(.{}, handleTcpConnection, .{ socket, client_address });
     }
 }
@@ -35,12 +36,15 @@ fn handleTcpConnection(socket: posix.socket_t, client_address: net.Address) !voi
     defer posix.close(socket);
     var buf: [128]u8 = undefined;
     while (true) {
-        const read = try posix.read(socket, &buf);
-        if (read == 0) {
+        const read_len = try posix.read(socket, &buf);
+        if (read_len == 0) {
             std.debug.print("{} disconnected\n", .{client_address});
             break;
         }
-        _ = try posix.write(socket, buf[0..read]);
+        std.debug.print("TCP read {} bytes\n", .{read_len});
+
+        const write_len = try posix.write(socket, buf[0..read_len]);
+        std.debug.print("TCP write {} bytes\n", .{write_len});
     }
 }
 
@@ -52,17 +56,20 @@ fn listenUdp(address: net.Address) !void {
 
     try posix.setsockopt(socket, posix.SOL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
     try posix.bind(socket, &address.any, address.getOsSockLen());
+    std.debug.print("UDP server started on {}\n", .{address});
 
     var buf: [128]u8 = undefined;
     while (true) {
         var client_addr: posix.sockaddr = undefined;
         var client_addr_len: posix.socklen_t = @sizeOf(posix.sockaddr);
 
-        const read = try posix.recvfrom(socket, &buf, 0, &client_addr, &client_addr_len);
-        if (read == 0) {
+        const read_len = try posix.recvfrom(socket, &buf, 0, &client_addr, &client_addr_len);
+        if (read_len == 0) {
             continue;
         }
+        std.debug.print("UDP read {} bytes\n", .{read_len});
 
-        _ = try posix.sendto(socket, buf[0..read], 0, &client_addr, client_addr_len);
+        const sendto_len = try posix.sendto(socket, buf[0..read_len], 0, &client_addr, client_addr_len);
+        std.debug.print("UDP write {} bytes\n", .{sendto_len});
     }
 }
